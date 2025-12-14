d30671db74d4131e339d9a788eff377ba8146491
haidukm95-byte <haidukm95@gmail.com>
2025-12-14 23:12:18 +0200

    Day 2 self-paced learning exercises

---
+++ Ex_6.py
@@ -0,0 +1,4 @@
+a=int(input("Type value A "))
+b=int(input("Now type value B "))
+if a > b:
+    print("Hello World")
---
+++ Ex_7.py
@@ -0,0 +1,5 @@
+a=int(input("Type any number here "))
+if a % 2 == 0:
+    print(f"The number {a} is EVEN.")
+else: 
+    print(f"The number {a} is ODD.")
