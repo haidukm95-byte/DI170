52ad04c03dd45203e820427d065e70cf8889d14f
haidukm95-byte <haidukm95@gmail.com>
2026-01-22 21:28:25 +0200

    Modify Countries table schema
    Removed UNIQUE constraint from the 'name' column in the Countries table.

--- create_table.sql
+++ create_table.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE Countries;
 CREATE TABLE Countries(
-    name VARCHAR(100) UNIQUE NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
     capital VARCHAR(100) NOT NULL, 
     flag TEXT NOT NULL, 
     subregion VARCHAR(200), 
     population INTEGER NOT NULL
     );
+
 
 
