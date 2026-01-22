47f32097d88273db721332e41ada871b1d3c510f
haidukm95-byte <haidukm95@gmail.com>
2026-01-22 19:06:06 +0200

    Add files via upload

--- W6D2DC.py
+++ W6D2DC.py
@@ -14,39 +14,29 @@ api_url = 'https://restcountries.com/v3.1/all?fields=name,capital,flag,subregion
 
 response = requests.get(api_url)
 if response.ok:
-    countries = response.json() 
-    random_country=random.choice(countries)
-    name = random_country['name']['common']  # name is nested!
-    capital = random_country.get('capital', ['N/A'])[0]  # capital is a list
-    flag = random_country['flag']
-    subregion = random_country.get('subregion', 'N/A')
-    population = random_country['population']
-    #full_list=[name, capital, flag, subregion, population]
-    #print(insert)
-
-else: 
+    countries = response.json()
+    
+    def insert_10(countries_list):
+        # Select 10 unique random countries
+        random_countries = random.sample(countries_list, min(10, len(countries_list)))
+        
+        for random_country in random_countries:
+            name = random_country['name']['common']
+            capital = random_country.get('capital', ['N/A'])[0]
+            flag = random_country['flag']
+            subregion = random_country.get('subregion', 'N/A')
+            population = random_country['population']
+            
+            query = """INSERT INTO Countries (name, capital, flag, subregion, population) VALUES (%s, %s, %s, %s, %s)"""
+            cursor.execute(query, (name, capital, flag, subregion, population))
+            print(f"The country {name} is added to the Countries database")
+        
+        connection.commit()  # Commit once after all inserts
+    
+    insert_10(countries)
+    
+else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
 
-def insert(name, capital, flag, subregion, population):
-    query = """INSERT INTO Countries (name, capital, flag, subregion, population) VALUES (%s, %s, %s, %s, %s)"""
-    cursor.execute(query, (name, capital, flag, subregion, population))
-    connection.commit()
-    print(f"The country {name} is added to the Countries database")        
-    
-def insert_10():
-    n=1
-    while n<=10:
-        random_country = random.choice(countries)
-        name = random_country['name']['common']
-        capital = random_country.get('capital', ['N/A'])[0]
-        flag = random_country['flag']
-        subregion = random_country.get('subregion', 'N/A')
-        population = random_country['population']
-    
-        insert(name, capital, flag, subregion, population)
-        n=n+1
-
-insert_10()
-
-
-
+cursor.close()
+connection.close()
