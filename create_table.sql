98b55e5715e5dfd837ea4094aaf6675b3fd3ba56
haidukm95-byte <haidukm95@gmail.com>
2026-01-22 21:27:45 +0200

    Adding 'Unique' statement to 'name' column

--- create_table.sql
+++ create_table.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE Countries;
 CREATE TABLE Countries(
-    name VARCHAR(100) NOT NULL, 
+    name VARCHAR(100) UNIQUE NOT NULL, 
     capital VARCHAR(100) NOT NULL, 
     flag TEXT NOT NULL, 
     subregion VARCHAR(200), 
     population INTEGER NOT NULL
     );
+
 
