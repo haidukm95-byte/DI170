f0c704803952024bbc9630e8f9395034d38d6e1e
haidukm95-byte <haidukm95@gmail.com>
2026-01-22 19:45:41 +0200

    Create database and table for Countries

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,9 @@
+CREATE DATABASE Countries;
 CREATE TABLE Countries(
     name VARCHAR(100) NOT NULL, 
     capital VARCHAR(100) NOT NULL, 
     flag TEXT NOT NULL, 
     subregion VARCHAR(200), 
     population INTEGER NOT NULL
+
     );
