b23a42b964e21e63bb624c1bb8565544cb3eed05
haidukm95-byte <haidukm95@gmail.com>
2026-02-15 22:03:32 +0200

    Week 9 Day 4 DailyChallenge fix

--- W9D4DC.js
+++ W9D4DC.js
@@ -36,18 +36,20 @@ const gameInfo = [
  //   const usernames = ["john!", "becky!", "susy!", "tyson!"]
 const usernames=[]
 gameInfo.forEach(val=>{
-    console.log(`${val.username}!`)
+    usernames.push(val.username+'!')
 })
+console.log(usernames)
 
 //2. Create an array using forEach that contains the usernames of all players with a 
 // score bigger than 5.
     //The new array should look like this :
 
     //const winners = ["becky", "susy"]
-const winners=gameInfo.filter(val=>val.score>5).forEach(val=>{
-    console.log(val.username)
+const winners=[]
+gameInfo.filter(val=>val.score>5).forEach(val=>{
+    winners.push(val.username)
 })
-
+console.log(winners)
 //3. Find and display the total score of the users. (Hint: The total score is 71)
 const totalScore=gameInfo.map(val=>val.score).reduce((acc, val)=>{
     return acc+val
